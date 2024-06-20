4c3d69d707e49cd4a4b2eddf57a28bb27010bd3d
Lucas Viggiano <viggiano.lucas@gmail.com>
2024-06-20 11:51:00 -0300

    login

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,5 +1,7 @@
 /// <reference types="cypress" />
 let dadosLogin
+const perfil = require('/repositorio/teste-e2e-ebac/cypress/fixtures/perfil.json')
+
 
 context('Funcionalidade Login', () => {
     before(() => {
@@ -36,23 +38,29 @@ context('Funcionalidade Login', () => {
         cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, aluno_ebac')
     })
 
-    it.only('deve exibir uma mensagem de erro ao inserir usuário invalido', () => {
+
+    it('Deve fazer login - utilizando massa de dados', () => {
+        cy.get('#username').type(perfil.usuario)
+        cy.get('#password').type(perfil.senha)
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('exist')
+
+    });
+    it('Deve exibir uma mensagem de erro ao inserir usuário invalido', () => {
 
         cy.get('#username').type('lucas.tete@teste.com')
-        cy.get('#password').type('teste123')
+        cy.get('#password').type('teste123', { log: false })
         cy.get('.woocommerce-form > .button').click()
         cy.get('.woocommerce-error').should('contain', 'Endereço de e-mail desconhecido. Verifique novamente ou tente seu nome de usuário.')
     });
 
-    it.only('deve exibir uma mensagem de erro ao inserir senha inválida', () => {
+    it('Deve exibir uma mensagem de erro ao inserir senha inválida', () => {
 
         cy.get('#username').type('lucas.teste@teste.com')
-        cy.get('#password').type('teste1234')
+        cy.get('#password').type('teste1234', { log: false })
         cy.get('.woocommerce-form > .button').click()
         cy.get('.woocommerce-error > li').should('contain', 'Erro: A senha fornecida para o e-mail lucas.teste@teste.com está incorreta. Perdeu a senha?')
 
     });
-
-
 
 })
