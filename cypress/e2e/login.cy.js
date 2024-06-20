b36987ec27f659e7d68c5dcf8d9170ebb82cf288
Lucas Viggiano <viggiano.lucas@gmail.com>
2024-06-20 11:05:23 -0300

    registro

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -28,11 +28,31 @@ context('Funcionalidade Login', () => {
         cy.get('.page-title').should('contain', 'Minha conta')
     });
 
-    it.skip('Deve fazer login com sucesso - sem otimização', () => {
+    it('Deve fazer login com sucesso - sem otimização', () => {
         cy.get('#username').type(dadosLogin.usuario)
         cy.get('#password').type(dadosLogin.senha, { log: false })
         cy.get('.woocommerce-form > .button').click()
         cy.get('.page-title').should('contain', 'Minha conta')
         cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, aluno_ebac')
     })
+
+    it.only('deve exibir uma mensagem de erro ao inserir usuário invalido', () => {
+
+        cy.get('#username').type('lucas.tete@teste.com')
+        cy.get('#password').type('teste123')
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.woocommerce-error').should('contain', 'Endereço de e-mail desconhecido. Verifique novamente ou tente seu nome de usuário.')
+    });
+
+    it.only('deve exibir uma mensagem de erro ao inserir senha inválida', () => {
+
+        cy.get('#username').type('lucas.teste@teste.com')
+        cy.get('#password').type('teste1234')
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.woocommerce-error > li').should('contain', 'Erro: A senha fornecida para o e-mail lucas.teste@teste.com está incorreta. Perdeu a senha?')
+
+    });
+
+
+
 })
---
+++ cypress/e2e/registro.cy.js
@@ -0,0 +1,45 @@
+/// <reference types="cypress" />
+import { faker } from '@faker-js/faker';
+
+
+describe('Funcionalidade: Cadastro', () => {
+    beforeEach(() => {
+       
+        cy.visit ('http://lojaebac.ebaconline.art.br/minha-conta/')
+    });
+
+
+    it('Deve realizar o cadastro com sucesso', () => {
+        cy.get('#reg_email').type(faker.internet.email())
+        cy.get('#reg_password').type('1234567890')
+        cy.get(':nth-child(4) > .button').click()
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('exist')
+
+    });
+    it('deve realizar o cadastro com sucesso - usando variáveis', () => {
+        var email = faker.internet.email()
+        var nome = faker.person.firstName()
+        var sobrenome = faker.person.lastName()
+
+        cy.get('#reg_email').type(email)
+        cy.get('#reg_password').type('1234567890')
+        cy.get(':nth-child(4) > .button').click()
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('exist')
+        cy.get('.woocommerce-MyAccount-navigation-link--edit-account > a').click()
+        cy.get('#account_first_name').type(nome)
+        cy.get('#account_last_name').type(sobrenome)
+        cy.get('.woocommerce-Button').click()
+        cy.get('.woocommerce-message').should('contain', 'Detalhes da conta modificados com sucesso.')
+
+    });
+    
+    it.only('Deve realizar o cadastro com sucesso - usando comando customizado', () => {
+        cy.preCadastro(faker.internet.email(), '1234567890', faker.person.firstName(), faker.person.lastName())
+        cy.get('.woocommerce-message').should('contain', 'Detalhes da conta modificados com sucesso.')
+
+    
+    
+    });
+
+
+});
