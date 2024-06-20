f86c6f8fc9f2453b31d9fd9d7283534a53caf462
Lucas Viggiano <viggiano.lucas@gmail.com>
2024-06-20 17:19:29 -0300

    teste e2e2

--- cypress/e2e/exercicio-e2e.cy.js
+++ cypress/e2e/exercicio-e2e.cy.js
@@ -1,22 +1,54 @@
 /// <reference types="cypress" />
+import produtosPage from "../support/page_objects/produtos.page";
+import { faker } from '@faker-js/faker';
 
 context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
-  /*  Como cliente 
-      Quero acessar a Loja EBAC 
-      Para fazer um pedido de 4 produtos 
-      Fazendo a escolha dos produtos
-      Adicionando ao carrinho
-      Preenchendo todas opções no checkout
-      E validando minha compra ao final */
+    /*  Como cliente 
+        Quero acessar a Loja EBAC 
+        Para fazer um pedido de 4 produtos 
+        Fazendo a escolha dos produtos
+        Adicionando ao carrinho
+        Preenchendo todas opções no checkout
+        E validando minha compra ao final */
 
-  beforeEach(() => {
-      cy.visit('/')
-  });
+    beforeEach(() => {
+        cy.visit('/')
+    });
 
-  it('Deve fazer um pedido na loja Ebac Shop de ponta a ponta', () => {
-      //TODO: Coloque todo o fluxo de teste aqui, considerando as boas práticas e otimizações
-      
-  });
+    it('Deve fazer um pedido na loja Ebac Shop de ponta a ponta', () => {
+        //TODO: Coloque todo o fluxo de teste aqui, considerando as boas práticas e otimizações
+        cy.get('.product-block > .block-inner >')
+            .eq(3)
+            .click()
+        cy.get('#tab-title-description > a').should('contain', 'Descrição')
+        cy.get('.button-variable-item-S').click()
+        cy.get('.button-variable-item-Blue').click()
+        cy.get('.single_add_to_cart_button').click()
+        produtosPage.buscarProduto('Apollo Running Short')
+        cy.get('.product_title').should('contain', 'Apollo Running Short')
+        produtosPage.addProdutoCarrinho('32', 'Black', 2)
+        cy.get('.single_add_to_cart_button').click()
+        cy.visit('produtos')
+        cy.get('.product-block > .block-inner >')
+            .first()
+            .click()
+        cy.get('.button-variable-item-L').click()
+        cy.get('.button-variable-item-Green').click()
+        cy.get('.input-text').clear().type(3)
+        cy.get('.single_add_to_cart_button').click()
+        produtosPage.visitarProduto('arcadio-gym-short')
+        cy.get('.button-variable-item-33').click()
+        cy.get('.button-variable-item-Red').click()
+        cy.get('.single_add_to_cart_button').click()
+        cy.get('.woocommerce-message > .button').click()
+        cy.get('.actions > .clearfix > .pull-right').click
+        cy.get('.checkout-button').click()
+        cy.checkout(faker.person.firstName(), faker.person.lastName(), 'Rua teste 123', 'São Paulo', '36420000', '1199999999', faker.internet.email())
+        cy.get('#terms').click()
+        cy.get('#createaccount').click()
+        cy.get('#account_password').type('teste123')
+        cy.get('#place_order').click()
+        cy.get('.page-title').should('exist')
 
-
+    });
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,17 @@ cy.get('#account_display_name').type(usuario)
 cy.get('.woocommerce-Button').click()
 
 })
+
+Cypress.Commands.add('checkout' , (nome, sobrenome, endereço, cidade, cep, tel, email, senha) =>   {   
+    cy.get('#billing_first_name').type(nome)
+    cy.get('#billing_last_name').type(sobrenome)
+    cy.get('#billing_address_1').type(endereço)
+    cy.get('#billing_city').type(cidade)
+    cy.get('#billing_postcode').type(cep)
+    cy.get('#billing_phone').type(tel)
+    cy.get('#billing_email').type(email)
+    
+
+
+
+    })
